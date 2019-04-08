294f543f7a57fd831e78eb074a29f7084042a2eb
v_wadcha <v_wadcha@ctlsd.set>
2019-04-08 15:40:21 +0700

    use token

--- route/route.go
+++ route/route.go
@@ -70,11 +70,11 @@ func Init() *echo.Echo {
 	// API group
 	r := e.Group("/api")
 	//Configure middleware with the custom claims type
-	//config := middleware.JWTConfig{
-	//	Claims:     &models.JWTCustomClaims{},
-	//	SigningKey: []byte("secret"),
-	//}
-	//r.Use(middleware.JWTWithConfig(config))
+	config := middleware.JWTConfig{
+		Claims:     &models.JWTCustomClaims{},
+		SigningKey: []byte("secret"),
+	}
+	r.Use(middleware.JWTWithConfig(config))
 
 	// Routes
 	r.GET("/reit", api.GetReitAllProcess)
